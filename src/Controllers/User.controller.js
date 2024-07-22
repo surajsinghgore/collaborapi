e05401db69e8f41c804f6fd67276b12c33158530
suraj singh rajput <surajthakurrs45@gmail.com>
2024-07-22 08:57:40 +0530

    user functions created

---
+++ src/Controllers/User.controller.js
@@ -0,0 +1,6 @@
+const UserData=require('../models/UserData.model');
+
+
+// const accessToken=UserData.generateAccessToken()
+// const refreshToken=UserData.generateRefreshToken()
+// const isPasswordValid = await user.isPasswordCorrect(password)
